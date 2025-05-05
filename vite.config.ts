425b64b358937412d97f0676fd95e02c112dc2cb
Malin2002 <malin.tunheim@gmail.com>
2025-05-06 00:20:16 +0200

    backend setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Forelesning08",
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
 });
